ae4415b97b49d78e77c2d3865cfb77a9cc54f493
Juan Ruiz <jrmotta119@gmail.com>
2021-10-19 21:21:49 -0400

    Message almost complete

--- Message.java
+++ Message.java
@@ -1,32 +1,106 @@
 import java.lang.Object;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 public class Message {
-    int messageLenght;
+    String messageLenght;
     byte[] messageLenghtBits;
     
-    ByteBuffer bbuf = ByteBuffer.allocate(4);
+    ByteBuffer bBufMessage = ByteBuffer.allocate(4);
+    ByteBuffer bBufPayload;
     int mPayload;
-    enum messageType { 
-		choke(0),
-		unchoke(1),
-		interested(2),
-		notInterested(3),
-		have(4),
-		bitfield(5),
-		request(6),
-		piece(7)
-        
-        int value;
-        messageType(int v){
-            value = v;
-        }
+    private static Charset charset = Charset.forName("US-ASCII");
+    public enum MessageType { 
+		choke,
+		unchoke,
+		interested,
+		notInterested,
+		have,
+		bitfield,
+		request,
+		piece
 	}
-    Message(int m){
-        messageLenght = m;
+    MessageType msgType;
+    Message(int m,int mType){
+        switch(mType){
+            case 0:
+                msgType = MessageType.choke;
+                break;
+            case 1:
+                msgType=MessageType.unchoke;
+                break;
+            case 2:
+                msgType=MessageType.interested;
+                break;
+            case 3:
+                msgType=MessageType.notInterested;
+                break;
+            case 4:
+                msgType=MessageType.have;
+                break;
+            case 5:
+                msgType=MessageType.bitfield;
+                break;
+            case 6:
+                msgType=MessageType.request;
+                break;
+            case 7:
+                msgType=MessageType.piece;
+                break;
+        }
+        
+        messageLenght = String.valueOf(m);
+        System.out.println(messageLenght);
         messageLenghtBits = messageLenght.getBytes();
-        bbuf.wrap(messageLenghtBits);
+        bbuf.put(messageLenghtBits);
     }
     void getMessagePayload(){
-        if(messageType)
+        switch(msgType){
+            case choke:
+                System.out.println("choke");
+                bBufPayload = ByteBuffer.allocate(0);
+                break;
+            case unchoke:
+                System.out.println("unchoke");
+                bBufPayload = ByteBuffer.allocate(0);
+                break;
+            case interested:
+                System.out.println("interested");
+                bBufPayload = ByteBuffer.allocate(0);
+                break;
+            case notInterested:
+                System.out.println("notInterested");
+                bBufPayload = ByteBuffer.allocate(0);
+                break;
+            case have:
+                System.out.println("have");
+                bBufPayload = ByteBuffer.allocate(4);
+                break;
+            case bitfield:
+                System.out.println("bitfield");
+                //TODO depends on peer
+                break;
+            case request:
+                System.out.println("request");
+                bBufPayload = ByteBuffer.allocate(4);
+                break;
+            case piece:
+                System.out.println("piece");
+                bBufPayload = ByteBuffer.allocate(4);
+                break;
+        }
     }
+    void getByteBuffer(){
+        String s;
+        if(bbuf.hasArray()){
+            s = new String(bbuf.array(), charset);
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args){
+        Message msg = new Message(3,7);
+        msg.getMessagePayload();
+        msg.getByteBuffer();
+    }
+    
 }
